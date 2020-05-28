de7fdfbf28355f1e032598d7b33a46dcf77827d2
inakimtz <36936424+inakimtz@users.noreply.github.com>
2020-05-28 09:35:14 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Enable service
-# sysrc -f /etc/rc.conf grafana_enable="YES"
+sysrc -f /etc/rc.conf grafana_enable="YES"
 
 # Start service
-# service grafana start 2>/dev/null
+service grafana start 2>/dev/null
